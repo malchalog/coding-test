689a8aa4c488d66b27c5e7f65b78a4008aee3118
양소연 <141055063+malchalog@users.noreply.github.com>
2023-08-15 16:09:56 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,9 +1,12 @@
-SELECT i.REST_ID, i.REST_NAME,i.FOOD_TYPE
-      ,i.FAVORITES AS FAVORITES, i.ADDRESS
-      ,ROUND(AVG(r.REVIEW_SCORE),2) AS SCORE
-FROM rest_review AS r
-     INNER JOIN rest_info AS i ON i.rest_id = r.rest_id
+SELECT i.REST_ID
+       ,i.REST_NAME
+       ,i.FOOD_TYPE
+       ,i.FAVORITES
+       ,i.ADDRESS
+       ,ROUND(AVG(r.REVIEW_SCORE),2) AS SCORE
+FROM rest_info  AS i
+     INNER JOIN rest_review AS r ON i.rest_id = r.rest_id #처음에 left join으로 했다가 리뷰가 있는 애들만 구해서 평균을 처리해야 하니까 inner join
+WHERE address like '서울%'
 GROUP BY rest_id
-HAVING address like '서울%'
-ORDER BY avg(r.review_score) DESC
-        ,favorites DESC
+ORDER BY 6 DESC
+        ,4 DESC
