649927df1a85fff7bb00ef317d847880b1631e81
양소연 <141055063+malchalog@users.noreply.github.com>
2023-08-19 23:59:51 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,7 +1,8 @@
--- 코드를 입력하세요
-SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS 'COUNT'
+SET @h = -1; # 0시부터 출력하기 위해 변수 선언
+
+SELECT (@h := @h + 1) as HOUR
+       , (SELECT COUNT(*) 
+          FROM animal_outs 
+          WHERE HOUR(DATETIME) = @h) as COUNT #변수와 일치하는 시간이면 카운트
 FROM ANIMAL_OUTS
-GROUP BY HOUR
-ORDER BY HOUR
-
-
+WHERE @h < 23 #최종 @h는 +1 더해서 23시
