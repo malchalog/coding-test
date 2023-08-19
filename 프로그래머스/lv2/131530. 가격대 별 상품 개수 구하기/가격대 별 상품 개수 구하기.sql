d7a102a452a0b2b19c9521efc68c27233050406a
양소연 <141055063+malchalog@users.noreply.github.com>
2023-08-19 23:21:13 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,30 +1,25 @@
-/*SELECT  CASE WHEN PRICE < 10000 then 0
-             WHEN 10000 <= PRICE and PRICE < 20000 then 10000
-             WHEN 20000 <= PRICE and PRICE < 30000 then 20000
-             WHEN 30000 <= PRICE and PRICE < 40000 then 30000
-             WHEN 40000 <= PRICE and PRICE < 50000 then 40000
-             WHEN 50000 <= PRICE and PRICE < 60000 then 50000
-             WHEN 60000 <= PRICE and PRICE < 70000 then 60000
-             WHEN 70000 <= PRICE and PRICE < 80000 then 70000
-             WHEN 80000 <= PRICE and PRICE < 90000 then 80000
-             END AS PRICE_GROUP
-        , count(*)
-  FROM  PRODUCT 
- GROUP BY  PRICE_GROUP
- ORDER BY  PRICE_GROUP ASC */
+-- 0818 
+/* SELECT  (CASE WHEN price < 10000 then 0
+             WHEN  price < 20000 then 10000
+             WHEN  price < 30000 then 20000
+             WHEN  price < 40000 then 30000
+             WHEN  price < 50000 then 40000
+             WHEN  price < 60000 then 50000
+             WHEN  price < 70000 then 60000
+             WHEN  price < 80000 then 70000
+             WHEN  price < 90000 then 80000
+             END) AS PRICE_GROUP
+        , count(*) AS PRODUCTS
+  FROM  product 
+ GROUP BY  price_group
+ ORDER BY  1 */
+ 
 
-SELECT  CASE WHEN PRICE < 10000 then 0
-             WHEN PRICE < 20000 then 10000
-             WHEN PRICE < 30000 then 20000
-             WHEN PRICE < 40000 then 30000
-             WHEN PRICE < 50000 then 40000
-             WHEN PRICE < 60000 then 50000
-             WHEN PRICE < 70000 then 60000
-             WHEN PRICE < 80000 then 70000
-             WHEN PRICE < 90000 then 80000
-             END AS PRICE_GROUP
-        , count(*)
-  FROM  PRODUCT 
- GROUP BY  PRICE_GROUP
- ORDER BY  PRICE_GROUP ASC 
 
+SELECT TRUNCATE(PRICE, -4) AS PRICE_GROUP #10000원 단위로 절삭 
+       , COUNT(*) AS PRODUCTS
+FROM PRODUCT
+GROUP BY 1
+ORDER BY 1
+
+ 
