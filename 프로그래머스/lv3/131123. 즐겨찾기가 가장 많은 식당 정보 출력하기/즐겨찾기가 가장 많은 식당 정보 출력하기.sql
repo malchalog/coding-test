dd9fd800ea5b36852967e0e1f3e56e6c7b63398c
양소연 <141055063+malchalog@users.noreply.github.com>
2023-08-25 01:34:54 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -10,7 +10,7 @@ SELECT FOOD_TYPE
                                      GROUP BY FOOD_TYPE )
 ORDER BY FOOD_TYPE DESC*/
 
-SELECT food_type
+/*SELECT food_type
        ,rest_id
        ,rest_name
        ,favorites
@@ -21,4 +21,19 @@ WHERE (food_type,favorites) IN (
                                 FROM rest_info
                                 GROUP BY food_type )  
 GROUP BY food_type
-ORDER BY food_type DESC ;  
+ORDER BY food_type DESC ; */
+
+SELECT food_type
+       ,rest_id
+       ,rest_name
+       ,favorites
+FROM (
+SELECT food_type
+       ,rest_id
+       ,rest_name
+       ,favorites
+       , RANK () OVER (PARTITION BY  food_type ORDER BY favorites DESC) AS best
+FROM rest_info ) AS price_rank
+WHERE best = 1
+GROUP BY 1
+ORDER BY 1 DESC ;
