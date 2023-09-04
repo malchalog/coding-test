a383af27aac138c1cc6ae063fa447407d4cbe463
양소연 <141055063+malchalog@users.noreply.github.com>
2023-09-04 23:57:54 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,2 @@
-SELECT i.animal_id
-      ,i.name
-FROM animal_ins AS i
-     LEFT JOIN animal_outs AS o ON o.animal_id = i.animal_id
-WHERE o.datetime <= i.datetime
-ORDER BY i.datetime
-
-     
+-- 코드를 입력하세요
+SELECT
