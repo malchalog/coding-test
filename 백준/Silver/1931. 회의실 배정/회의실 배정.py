72659d991c6416617346484396405b8d323f83e8
양소연 <141055063+malchalog@users.noreply.github.com>
2024-06-16 16:41:54 +0900

    [Silver I] Title: 회의실 배정, Time: 204 ms, Memory: 66796 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,26 +1,30 @@
-# 입력 받기
 import sys
 input = sys.stdin.read
-data = input().split()
-n = int(data[0])
-meetings = []
 
-for i in range(1, 2*n+1, 2):
-    start = int(data[i])
-    end = int(data[i+1])
-    meetings.append((start, end))
+def main():
+    data = input().split()
+    n = int(data[0])
+    
+    meetings = []
+    for i in range(1, len(data), 2):
+        start_time = int(data[i])
+        end_time = int(data[i + 1])
+        meetings.append((start_time, end_time))
+    
+    # 끝나는 시간 기준으로 정렬, 끝나는 시간이 같으면 시작 시간 기준으로 정렬
+    meetings.sort(key=lambda x: (x[1], x[0]))
+    
+    # 회의실을 사용할 수 있는 최대 회의 개수 계산
+    count = 0
+    end_time = 0
+    
+    for start, end in meetings:
+        if start >= end_time:
+            end_time = end
+            count += 1
+    
+    # 결과 출력
+    print(count)
 
-# 회의를 끝나는 시간 기준으로 정렬
-meetings.sort(key=lambda x: (x[1], x[0]))
-
-# 회의 선택
-count = 0
-end_time = 0
-
-for meeting in meetings:
-    if meeting[0] >= end_time:
-        count += 1
-        end_time = meeting[1]
-
-# 결과 출력
-print(count)
+if __name__ == '__main__':
+    main()
